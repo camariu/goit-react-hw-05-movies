c01d6df3fced893f063b1bd9af85cb193ae8ddca
camariu <amariutei1998@gmail.com>
2023-09-24 11:39:40 +0300

    Update MoviesSearch.jsx

--- src/pages/MoviesSearch/MoviesSearch.jsx
+++ src/pages/MoviesSearch/MoviesSearch.jsx
@@ -1,10 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import style from './MoviesSearch.module.css';
-
-const API_URL =
-  'https://api.themoviedb.org/3/movie/popular?api_key=4d3dddf3002235eeb592ca83d8dffe0d';
-const API_Search =
-  'https://api.themoviedb.org/3/search/movie?api_key=4d3dddf3002235eeb592ca83d8dffe0d&language=en-US&page=1&include_adult=false&query';
 
 export default function MoviesSearch() {
   const [movie, setMovie] = useState([]);
@@ -31,15 +26,15 @@ export default function MoviesSearch() {
   return (
     <div className={style.searchB}>
       <div className={style.search}>
-      <form onSubmit={searchMovie}>
-        <input
-          value={query}
-          onChange={changeHandler}
-          type="text"
-          name="searchInput"
-        />
-        <button type="submit">Search</button>
-      </form>
+        <form onSubmit={searchMovie}>
+          <input
+            value={query}
+            onChange={changeHandler}
+            type="text"
+            name="searchInput"
+          />
+          <button type="submit">Search</button>
+        </form>
       </div>
       <div>
         {movie.length > 0 ? (
@@ -57,7 +52,7 @@ export default function MoviesSearch() {
             ))}
           </div>
         ) : (
-          <h2>Sorry !! No movie</h2>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+          <h2>Sorry !! No movie</h2>
         )}
       </div>
     </div>
